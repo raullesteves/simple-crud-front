85964aac0e840228536ea1cfd73361479a0912bb
Raul Esteves <raul@esteves.dev>
2019-10-08 07:54:08 -0300

    removing update component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,9 @@ import { UsersListComponent } from "./users/users-list/users-list.component";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms"
 import { UsersCreateComponent } from './users/users-create/users-create.component';
-import { UsersUpdateComponent } from './users/users-update/users-update.component';
 
 @NgModule({
-  declarations: [AppComponent, UsersListComponent, UsersCreateComponent, UsersUpdateComponent],
+  declarations: [AppComponent, UsersListComponent, UsersCreateComponent],
   imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/config/config.js
+++
@@ -1,3 +0,0 @@
-export default {
-    apiUrl: 'localhost:3000'
-}
